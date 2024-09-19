599ced8c4f01af638791dce4f445de35da9f5142
kargamant <firexholms@gmail.com>
2024-09-19 21:00:11 +0300

    Fixed multiplication itself. Also made testing

--- lab_1/matrix_vector_multiply.py
+++ lab_1/matrix_vector_multiply.py
@@ -4,11 +4,13 @@ from numba import cuda
 import numpy as np
 
 def verify(res, b, a):
-    return not True in [False in i for i in res==np.dot(b,a)]
+    return not False in [i for i in res==np.dot(b,a)]
 
 def matrix_vector_multiply(matrix, vector, shape):
-    result=np.empty_like(matrix)
-    for i in range(shape[1]):
+    result=np.empty_like(vector)
+    for i in range(len(vector)):
+        result[i]=0
         for j in range(shape[0]):
-            result[i][j]+=matrix[i][j]*vector[j]
+            result[i]+=matrix[i][j]*vector[j]
+            #print(result)
     return result
--- lab_1/testing.py
+++ lab_1/testing.py
@@ -5,10 +5,7 @@ import random
 shape=(5, 5)
 
 #vector
-a=np.arange(0, shape[0]).astype(np.float16)
-
-#matrix
-b=np.ones((shape[1], shape[0]), dtype=np.float16)
+a=np.arange(0, shape[0]).astype(np.int32)
 
 def gen_int_matr(shape, start, end):
     b=np.empty(shape, dtype=np.int16)
@@ -24,19 +21,15 @@ def gen_float_matr(shape, start, end):
             b[i][j]=random.uniform(start, end)
     return b
 
-b=gen_float_matr(shape, 0.0, 1.0)
-res=matrix_vector_multiply(b, a, shape)
-print("b:\n", b)
-print("a:\n", a)
-print("res:\n",res)
-print("verify: ", verify(res.astype(np.float16), b, a))
-
 #testing
-# results=[]
-# for _ in range(0, 100):
-#     for i in range(shape[1]):
-#         for j in range(shape[0]):
-#             b[i][j] = random.uniform(0.0, 1.0)
-#     res = matrix_vector_multiply(b.astype(np.float16), a.astype(np.float16), shape)
-#     results.append(verify(res, b, a))
-# print("testing results: ", results.count(True), " out of ", len(results))
+results=[]
+for _ in range(0, 10000):
+    b=gen_int_matr(shape, 0, 500)
+    res = matrix_vector_multiply(b, a, shape)
+    results.append(verify(res, b, a))
+    if results[-1]==False:
+        print("b:\n", b)
+        print("a:\n", a)
+        print("res:\n", res)
+        print("dot: \n", np.dot(b, a))
+print("testing results: ", results.count(True), " out of ", len(results))
