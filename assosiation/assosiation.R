b3a27818139ab6b7fa7d9600a0d10e9d1e7800c8
kargamant <firexholms@gmail.com>
2024-11-18 20:56:56 +0300

    Inspected top 40 rules by conf. Made matrix visualisation to see top lift rules and top conf rules

--- assosiation/assosiation.R
+++ assosiation/assosiation.R
@@ -20,8 +20,66 @@ inspection <- inspect(rules)
 # 1165 rules
 
 library(ggplot2)
+library(plotly)
 
-plt <- ggplot(inspection, aes(unlist(inspection["support"]), unlist(inspection["confidence"]), colour = unlist(inspection["lift"])))
-plt + geom_point() + labs(x="support", y="confidence")
+plt <- ggplot(inspection, aes(
+  text=paste(unlist(inspection["lhs"]), unlist(inspection["rhs"]), sep="->"), 
+  unlist(inspection["support"]), 
+  unlist(inspection["confidence"]), 
+  colour = unlist(inspection["lift"])))
+scatter_plot <- plt + geom_point() + labs(x="support", y="confidence", color="lift")
+ggplotly(scatter_plot)
+ggsave("support_confidence_plt.png")
 
+plt <- ggplot(inspection, aes(
+  text=paste(unlist(inspection["lhs"]), unlist(inspection["rhs"]), sep="->"), 
+  unlist(inspection["support"]), 
+  unlist(inspection["lift"]), ))
+scatter_plot <- plt + geom_point() + labs(x="support", y="lift")
+ggplotly(scatter_plot)
+ggsave("support_lift_plt.png")
+
+rules <- apriori(tr, parameter=list(supp=0.01, conf=0.8))
+rules <- sort(rules, by="lift")
+inspection <- inspect(rules)
+
+plt <- ggplot(inspection,
+              aes(
+                text=unlist(inspection["lift"]),
+                unlist(inspection["lhs"]),
+                unlist(inspection["rhs"]),
+                #colour = unlist(inspection["lift"]),
+                fill = unlist(inspection["lift"])
+              ))
+matrix_based_plt <- plt + 
+  geom_raster() + 
+  labs(x="lhs", y="rhs", fill="lift") + 
+  theme(axis.text.x = element_blank(), axis.text.y = element_blank())
+ggplotly(matrix_based_plt)
+ggsave("matrix_based_viz_lift_plt.png")
+# top lift rules
+# {item15, item30, item49} -> {item56}
+# {item15, item49} -> {item56}
+# {item30, item49, item84} -> {item56}
+# {item30, item49, item56} -> {item15}
+
+plt <- ggplot(inspection,
+              aes(
+                text=unlist(inspection["confidence"]),
+                unlist(inspection["lhs"]),
+                unlist(inspection["rhs"]),
+                #colour = unlist(inspection["lift"]),
+                fill = unlist(inspection["confidence"])
+              ))
+matrix_based_plt <- plt + 
+  geom_raster() + 
+  labs(x="lhs", y="rhs", fill="confidence") + 
+  theme(axis.text.x = element_blank(), axis.text.y = element_blank())
+ggplotly(matrix_based_plt)
+ggsave("matrix_based_viz_conf_plt.png")
+# top confidence rules
+# {item15, item49, item56} -> {item30}
+# {item49, item56, item84} -> {item30}
+# {item49, item56} -> {item30}
+# {item15, item49, item84} -> {item30}
 
