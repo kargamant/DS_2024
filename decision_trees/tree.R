a6b80657a3545c613ac2826b554a1debc2fcc297
kargamant <firexholms@gmail.com>
2024-12-10 10:21:10 +0300

    Add last part with pruning of tree

--- decision_trees/tree.R
+++ decision_trees/tree.R
@@ -34,3 +34,29 @@ auc
 
 pr <- predict(tree, test, type="class")
 confusionMatrix(as.factor(unlist(pr)), as.factor(unlist(test["MYDEPV"])))
+
+tree_gini <- rpart(MYDEPV ~ Price + Income + Age, data=train, method="class", parms=list(split="gini"), minsplit=3)
+fancyRpartPlot(tree_gini)
+pr <- predict(tree_gini, train, type="class")
+confusionMatrix(as.factor(unlist(pr)), as.factor(unlist(train["MYDEPV"])))
+
+pr <- predict(tree_gini, train, type="vector")
+
+labels <- unlist(train["MYDEPV"])
+labels <- replace(labels, labels==1, 2)
+labels <- replace(labels, labels==0, 1)
+
+probj <- prediction(pr, as.factor(labels))
+perf <- performance(probj, "tpr", "fpr")
+plot(perf)
+auc <- auc_curve@y.values[[1]]
+auc
+
+pr <- predict(tree_gini, test, type="class")
+confusionMatrix(as.factor(unlist(pr)), as.factor(unlist(test["MYDEPV"])))
+printcp(tree_gini)
+pruned_tree_gini <- prune(tree_gini, cp=0.03)
+pruned_tree_gini
+
+pr <- predict(pruned_tree_gini, train, type="class")
+confusionMatrix(as.factor(unlist(pr)), as.factor(unlist(train["MYDEPV"])))
