acaf2d0b4fa60baed2316c8ff4a20f6e7dee30d2
kargamant <firexholms@gmail.com>
2024-12-14 18:10:59 +0300

    tried different model params. Picked appropriate ones

--- arima/arima.R
+++ arima/arima.R
@@ -6,10 +6,39 @@ series
 plot(series)
 axis(1, 1960:1980)
 
-plot(diff(series))
+plot(diff(series, differences=1))
 axis(1, 1960:1980)
 
 log_series <- ts(log(data[["EPS"]]), frequency=4, start=c(1960,1))
 plot(log_series)
 
-plot(diff(log_series))
+stationary <- diff(log_series, differences=1) 
+plot(stationary)
+
+# non differenced
+acf(series, 83)
+axis(1, 1:83)
+pacf(series, 83)
+axis(1, 1:83)
+
+acf(stationary, 83)
+axis(1, 1:83)
+pacf(stationary, 83)
+axis(1, 1:83)
+
+
+
+# I'd say that AR(1), I(1), MA(5)
+model <- arima(stationary, order=c(1,1,5))
+model["aic"]
+
+# looking for the lowest aic
+p <- 1
+d <- 1
+q <- c(1:10)
+stationary <- diff(log_series, differences=d) 
+
+aics <- sapply(q, function(x) arima(stationary, order=c(p,d,x))["aic"])
+aics
+
+
