1783d3da7245c3522442986139d2c1524c90cac7
kargamant <firexholms@gmail.com>
2024-12-19 13:43:20 +0300

    Fixed nnCostFunction

--- mlclass-ex4/nnCostFunction.m
+++ mlclass-ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -46,12 +46,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -62,21 +62,71 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+a1 = [ones(m, 1) X];
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+
+a2 = [ones(m, 1) a2];
+
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
+h = a3;
+
+y_matrix = eye(num_labels)(y,:);
+
+J = sum(sum((-y_matrix) .* log(h) - (1 - y_matrix) .* log(1 - h))) / m;
+J = J + (lambda / (2*m)) * (sum(sum(Theta1(:,2:end) .^ 2)) + sum(sum(Theta2(:,2:end) .^ 2)));
+
+for t = 1:m
+  % Step 1: Forward pass (already done above)
+  a1 = [1; X(t, :)'];
+  z2 = Theta1 * a1;
+  a2 = [1; sigmoid(z2)];
+  z3 = Theta2 * a2;
+  a3 = sigmoid(z3);
+
+  % Step 2: Calculate error terms
+  delta3 = a3 - y_matrix(t, :)';
+  delta2 = (Theta2' * delta3)(2:end) .* sigmoidGradient(z2);
+
+  % Step 3: Accumulate gradients
+  % delta(i) - opposite direction to what we want to change
+  % a(i-1) - magnitude on what to change
+  Theta1_grad = Theta1_grad + delta2 * a1';
+  Theta2_grad = Theta2_grad + delta3 * a2';
+endfor
+
+% Step 4: Divide by m and add regularization
+Theta1_grad = (1/m) * Theta1_grad  + (lambda/m) * [zeros(size(Theta1,1),1) Theta1(:,2:end)];
+Theta2_grad = (1/m) * Theta2_grad + (lambda/m) * [zeros(size(Theta2,1),1) Theta2(:,2:end)];
 
 
+%for t = 1:m
+%  delta3 = a3(t, :)' - y_matrix(t, :)';
+%  delta2 = (Theta2(:, 2:end)' * delta3) .* sigmoidGradient(z2(t, :)');
 
+%  Theta1_grad = Theta1_grad + delta2 * a1(t, :);
+%  Theta2_grad = Theta2_grad + delta3 * a2(t, :);
+%endfor
 
+%Theta1_grad = (1/m) * Theta1_grad + (lambda/m) * [zeros(size(Theta1,1),1) Theta1(:,2:end)];
+%Theta2_grad = (1/m) * Theta2_grad + (lambda/m) * [zeros(size(Theta2,1),1) Theta2(:,2:end)];
 
+%for t=1:m
+  %a1=X(t:t,:);
+  %a1 = [ones(m, 1) a1];
+  %z2 = Theta1 * a1';
 
+  %a2 = sigmoid(z2);
+  %a2 = [a2; ones(1, size(a2, 2))];
+  %z3 = Theta2 * a2;
+  %a3 = sigmoid(z3);
 
+  % back propagation
+  %delta3 = a3 - y;
+  %delta2 = Theta2' * delta3 .* sigmoidGradient(z2);
 
-
-
-
-
-
-
-
+%endfor
 
 
 
