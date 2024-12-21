e3855e641177ca60e8f68a70efc3981ee7343899
kargamant <firexholms@gmail.com>
2024-12-21 17:02:54 +0300

    better code in bag of words and analytics of reuters article

--- nlp/bag_of_words.py
+++ nlp/bag_of_words.py
@@ -2,23 +2,50 @@ stop_words = []
 with open("stop_words.txt", 'r') as file:
     stop_words = file.read().split('\n')
 print(stop_words)
-content = ''
-with open('yoyodine_bank_review.txt', 'r') as file:
-    content = file.read().replace('\n', ' ').replace('!', '').replace('?', '').replace(',', '').replace('.', '').lower().split(' ')
-content.remove('')
+
+def read_content(filename: str):
+    with open(filename, 'r') as file:
+        content = file.read().replace('\n', ' ').replace('!', '').replace('?', '').replace(',', '').replace('.', '').lower().split(' ')
+    content = list(filter(lambda x: x!='', content))
+    return content
+
+def stop_words_remove(content, stop_words):
+    for st in stop_words:
+        if st in content:
+            content = list(filter(lambda x: x!=st, content))
+    return content
+
+def form_bag_of_words(content):
+    bag_of_words = {}
+    for w in content:
+        bag_of_words[w] = 0
+
+    for w in content:
+        bag_of_words[w]+=1
+    return bag_of_words
+
+content = read_content('yoyodine_bank_review.txt')
 print(content, end='\n\n')
 
-# removing stop words
-for st in stop_words:
-    if st in content:
-        content = list(filter(lambda x: x!=st, content))
+content = stop_words_remove(content, stop_words)
 print(content, end='\n\n')
 
-bag_of_words = {}
-for w in content:
-    bag_of_words[w] = 0
-
-for w in content:
-    bag_of_words[w]+=1
+bag_of_words = form_bag_of_words(content)
 
 print(bag_of_words)
+
+content = read_content('reuters.txt')
+content = stop_words_remove(content, stop_words)
+print(content, end='\n\n')
+
+bag_of_words = form_bag_of_words(content)
+top = [ item for item in sorted(bag_of_words.items(), key=lambda x: x[1], reverse=True)]
+print(top[:10:])
+
+
+
+
+
+
+
+
---
+++ nlp/nlp.R
@@ -0,0 +1,15 @@
+library("tm")
+library("wordcloud")
+data("crude")
+
+# top10 words(from python)
+# [('oil', 12), 
+# ('said', 11), 
+# ('opec', 10), 
+# ('production', 5), 
+# ('prices', 5), 
+# ('analysts', 4), 
+# ('problem', 4), 
+# ('mln', 4), 
+# ('demand', 4), 
+# ('may', 3)]
